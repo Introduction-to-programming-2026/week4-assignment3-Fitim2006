b48a6ea0278d82aec13e036849803fdfc602ea5c
Fitim Brava <fitim.brava@ibu.edu.mk>
2026-03-09 06:29:50 +0000

    Mon, Mar 9, 2026, 7:29 AM +01:00

--- src/phonebook.c
+++ src/phonebook.c
@@ -11,29 +11,26 @@ Person;
 
 int main(void)
 {
-    // Create an array of 5 persons
     Person people[5];
 
     // Fill the phonebook
-    people[0].name = "Alice";
+    people[0].name = "Fitim";
     people[0].phone = "111-1111";
 
-    people[1].name = "Bob";
+    people[1].name = "Ernik";
     people[1].phone = "222-2222";
 
-    people[2].name = "Charlie";
+    people[2].name = "Arlinda";
     people[2].phone = "333-3333";
 
-    people[3].name = "David";
+    people[3].name = "Mustafa";
     people[3].phone = "444-4444";
 
-    people[4].name = "Eve";
+    people[4].name = "Ema";
     people[4].phone = "555-5555";
 
-    // Ask user for a name
     string search = get_string("Name: ");
 
-    // Search the phonebook
     for (int i = 0; i < 5; i++)
     {
         if (strcmp(people[i].name, search) == 0)
@@ -43,6 +40,5 @@ int main(void)
         }
     }
 
-    // If not found
     printf("Not found\n");
 }
